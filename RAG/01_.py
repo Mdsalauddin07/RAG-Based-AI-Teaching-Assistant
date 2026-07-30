dc71aa66f6d7dfc890f2830bf93c46ccca6de2ee
Md Salauddin <mdmssallu4100@gmail.com>
2026-07-31 04:03:43 +0530

    Add files via upload

---
+++ RAG/01_.py
@@ -0,0 +1,22 @@
+
+# predict the output in 5 sec..
+
+num = [1, 2, 3, 4, 5]
+squared = list(map(
+    lambda x : x ** 2, num
+))
+
+print(squared)
+
+# Can you solve this
+# Comment your output
+# ok let's see the output
+# the output is
+# [1, 4, 9, 16, 25]
+# ok 
+
+
+
+
+
+
